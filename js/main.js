3933eabb24a32d7dabe6a0bbbc6af538ae8858dd
Igroman4ik <ni4000009@gmail.com>
2023-11-26 13:52:30 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
 const result = document.getElementById('result')
 const btn = document.getElementById('calculate')
-
-
-let math = document.querySelector('select')
+const math = document.querySelector('select')
 console.log(math.value)
-
-let num1 = document.getElementById('num1')
+const num1 = document.getElementById('num1')
 console.log(num1.value)
-
-let num2 = document.getElementById('num2')
+const num2 = document.getElementById('num2')
 console.log(num2.value)
+
+
+function color() {
+    if (result.textContent < 0) {
+        result.style.color = 'red'
+    } else {
+        result.style.color = 'green'
+    }
+}
+
 
 btn.onclick = function () {
 
     if (math.value == '+') {
         console.log(math.value)
         result.textContent = Number(num1.value) + Number(num2.value)
-    }
-    if (math.value == '-') {
+    } else if (math.value == '-') {
         console.log(math.value)
         result.textContent = Number(num1.value) - Number(num2.value)
-    }
-    if (math.value == "*") {
+    } else if (math.value == "*") {
         console.log(math.value)
         result.textContent = Number(num1.value) * Number(num2.value)
-    }
-    if (math.value == "/") {
+    } else {
         console.log(math.value)
         result.textContent = Number(num1.value) / Number(num2.value)
     }
 
-
-
+    color()
     console.log(result.textContent)
 }
 
